2b33d3bb3adc8735913e5c55b625f105c887cb86
Fábio Costa <fabiodanielmc@gmail.com>
2024-02-08 22:32:15 +0100

    start navbar with dark and light mode logic and colors

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,5 +1,89 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery,
+    Icon
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
-    return (<div>Navbar</div>)
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
+
+    const theme = useTheme();
+    const neutralLight = theme.pallete.neutral.light;
+    const dark = theme.pallete.neuitral.dark;
+    const background = theme.pallete.background;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap="1.75 rem">
+            <Typography 
+            fontWeight="bold" 
+            fontSize="clamp(1rem, 2rem, 2.25rem"
+            color="primary"
+            onclick={()=> navigate("/home")}
+            sx={{
+                "&:hover": {
+                    color: primaryLight,
+                    cursor: "pointer",
+                }
+            }}
+            >
+                SoMedia
+            </Typography>
+            {isNonMobileScreens && (
+                <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+                    <InputBase placeholder="Search..."/>
+                        <IconButton >
+                            <Search/>
+                        </IconButton>
+                </FlexBetween>
+            )}
+        </FlexBetween>
+
+        {/* Desktop Nav */}
+        
+        {isNonMobileScreens ? (
+            <FlexBetween gap="2rem">
+                <IconButton onclick = { () => dispatch(setMode())}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkMode sx={{ fontSize: "25px" }} />
+                    ) : (
+                        <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                    )}
+                    </IconButton>
+            </FlexBetween>
+            ) : (
+            <IconButton></IconButton>
+    )}
+    </FlexBetween>
 };
 
 export default Navbar;
