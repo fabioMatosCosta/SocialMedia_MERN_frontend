7e280a4f4753c7663bb940f1d4e0bdd2e23f4315
Fábio Costa <fabiodanielmc@gmail.com>
2024-02-14 22:35:23 +0100

    finish userwidget and add to homepage

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -3,6 +3,8 @@ import {
     EditOutlined,
     LocationOnOutlined,
     WorkOutlineOutlined,
+    Man,
+    Tune,
 } from "@mui/icons-material";
 import {
     Box,
@@ -45,4 +47,137 @@ const UserWidget = ({ userId, picturePath }) => {
     if(!user) {
         return null;
     }
+
+    const {
+        firstName,
+        lastName,
+        location,
+        occupation,
+        viewedProfile,
+        impressions,
+        friends,
+    } = user;
+
+    return(
+        <WidgetWrapper>
+            {/*Frist Row */}
+            <FlexBetween
+                gap="0.5rem"
+                pb="1.1rem"
+                onClick={()=> navigate(`/proflie/${userId}`)}
+            >
+                <FlexBetween gap="1rem">
+                    <UserImage image={picturePath}/>
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            color= {dark}
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer"
+                                }
+                            }}
+                        >
+                            {firstName} {lastName}
+                        </Typography>
+                        <Typography
+                            color={medium}
+                        >
+                            {friends.length} friends
+                        </Typography>
+                    </Box>
+                    <ManageAccountsOutlined />
+                </FlexBetween>
+
+                <Divider/>
+
+                {/*Second Row */}
+
+                <Box p="1rem 0">
+                    <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                        <LocationOnOutlined fontSize="large" sx={{ color:main }}/>
+                        <Typography color={medium}>{location}</Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                        <WorkOutlineOutlined fontSize="large" sx={{ color:main }}/>
+                        <Typography color={medium}>{occupation}</Typography>
+                    </Box>
+                </Box>
+
+                {/* Third Row */}
+                <Box p="1rem 0">
+                    <FlexBetween mb="0.5rem">
+                        <Typography color={medium}>
+                            Who's viewed your profile
+                        </Typography>
+                        <Typography 
+                            color={medium}
+                            fontWeight="500"
+                        >
+                            {viewedProfile}
+                        </Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                        <Typography color={medium}>
+                            Impressions of your post
+                        </Typography>
+                        <Typography 
+                            color={medium}
+                            fontWeight="500"
+                        >
+                            {impressions}
+                        </Typography>
+                    </FlexBetween>
+                </Box>
+
+                {/* Fourth Row */}
+
+                <Box p="1rem 0">
+                    <Typography
+                        fontSize="1rem"
+                        color={main}
+                        fontWeight="500"
+                        mb="1rem"
+                    >
+                        Social Profiles
+                    </Typography>
+
+                    <FlexBetween gap="1rem" mb="0.5rem">
+                        <FlexBetween gap="1rem">
+                            <img src="../assets/twitter.png" alt="twitter"/>
+                            <Box>
+                                <Typography color={main} fontWeight="500">
+                                    Twitter
+                                </Typography>
+                                <Typography color={medium}>
+                                    SociaL Network
+                                </Typography>
+                            </Box>
+                        </FlexBetween>
+                        <EditOutlined sx={{ color: main }}/>
+                    </FlexBetween>
+
+                    <FlexBetween gap="1rem">
+                        <FlexBetween gap="1rem">
+                            <img src="../assets/linkedin.png" alt="linkedin"/>
+                            <Box>
+                                <Typography color={main} fontWeight="500">
+                                    LinkedIn
+                                </Typography>
+                                <Typography color={medium}>
+                                    Network Platform
+                                </Typography>
+                            </Box>
+                        </FlexBetween>
+                        <EditOutlined sx={{ color: main }}/>
+                    </FlexBetween>
+                </Box>
+            </FlexBetween>
+        </WidgetWrapper>
+    )
 }
+
+
+export default UserWidget;
