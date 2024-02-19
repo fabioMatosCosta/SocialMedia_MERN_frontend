2d579cb14094b32b5500730f477b07d682f76fec
Fábio Costa <fabiodanielmc@gmail.com>
2024-02-19 23:31:47 +0100

    start MyPostWidget

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -3,25 +3,17 @@ import {
     EditOutlined,
     LocationOnOutlined,
     WorkOutlineOutlined,
-    Man,
-    Tune,
-} from "@mui/icons-material";
-import {
-    Box,
-    Typography,
-    Divider,
-    useTheme, 
-    getIconUtilityClass
-} from "@mui/material";
-import UserImage from "components/UserImage.jsx";
-import FlexBetween from "components/FlexBetween.jsx";
-import WidgetWrapper from "components/WidgetWrapper.jsx";
+    } from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserWidget = ({ userId, picturePath }) => {
-    const[user, setUser] =  useState(null);
+    const [user, setUser] = useState(null);
     const { palette } = useTheme();
     const navigate = useNavigate();
     const token = useSelector((state) => state.token);
@@ -30,21 +22,19 @@ const UserWidget = ({ userId, picturePath }) => {
     const main = palette.neutral.main;
 
     const getUser = async () => {
-        const response = await  fetch(`http://localhost:3001/users/${userId}`,
-            {
-                method: "GET",
-                headers: { Authorization: `Bearer ${token}`},
-            }
-        );
+        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+        });
         const data = await response.json();
         setUser(data);
     };
 
-    useEffect(()=> {
+    useEffect(() => {
         getUser();
-    }, []) //eslint-disable-line react-hooks/exhaustive-deps
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    if(!user) {
+    if (!user) {
         return null;
     }
 
@@ -57,7 +47,7 @@ const UserWidget = ({ userId, picturePath }) => {
         impressions,
         friends,
     } = user;
-
+    
     return(
         <WidgetWrapper>
             {/*Frist Row */}
