65d6b6d38c757853021b961f27ff300fc9484978
Fábio Costa <fabiodanielmc@gmail.com>
2024-02-24 12:42:36 +0100

    continue post Widget, api call for the likes functionality

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -7,17 +7,44 @@ import {
 import { Box, Divider, IconButton, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
-
-import { useEffect } from "react";
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "state";
 
-const PostWidget = ({ userId, isProfile = false}) => {
+const PostWidget = ({ 
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+}) => {
+    const [isComments, setIsComments] = useState(false);
     const dispatch = useDispatch();
-    const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
+    const likeCount = Object.keys(likes).length;
 
-    
+    const { palette } = useTheme();
+    const primary = palette.primary.main;
+    const main =  palette.neutral.main;
+
+    const patchLike = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ userId: loggedInUserId })
+        });
+        
+    }
 
 };
 
